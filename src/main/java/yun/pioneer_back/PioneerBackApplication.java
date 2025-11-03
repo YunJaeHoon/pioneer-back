6f628c259e7c607bb36a555dee05d8eba8686802
YunJaeHoon <sleepzzz214@naver.com>
2025-11-03 21:37:43 +0900

    project init

---
+++ src/main/java/yun/pioneer_back/PioneerBackApplication.java
@@ -0,0 +1,13 @@
+package yun.pioneer_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PioneerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PioneerBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yun/pioneer_back/PioneerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package yun.pioneer_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PioneerBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
