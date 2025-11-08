52485535b1ffec83aae885ed8c1d2045e3c7cb9a
YunJaeHoon <sleepzzz214@naver.com>
2025-11-08 18:30:01 +0900

    feat: 생성 시점과 수정 시점을 저장할 BaseEntity 구현

---
+++ src/main/java/yun/pioneer_back/common/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package yun.pioneer_back.common.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class BaseEntity
+{
+    // 생성 시점
+    @NotNull
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    // 수정 시점
+    @NotNull
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
