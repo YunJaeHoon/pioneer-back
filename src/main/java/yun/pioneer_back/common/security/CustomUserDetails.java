3f2a257cc844ec3d1143ee5a775e921050240cba
YunJaeHoon <sleepzzz214@naver.com>
2025-11-09 15:45:20 +0900

    feat: 커스텀 UserDetails 구현

---
+++ src/main/java/yun/pioneer_back/common/security/CustomUserDetails.java
@@ -0,0 +1,55 @@
+package yun.pioneer_back.common.security;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import yun.pioneer_back.common.entity.User;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+@Getter
+@EqualsAndHashCode
+public class CustomUserDetails implements UserDetails
+{
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
